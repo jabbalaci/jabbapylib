383922ee24d5b9458be3beff20a52bf4c1fcab96
Jabba Laci <jabba.laci@gmail.com>
2011-09-06 19:47:49 -0400

    README updated

---
+++ jabbapylib/say/hello.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+"""
+For test purposes.
+"""
+
+def hi():
+    print "jabbapylib works :)"
+
+#############################################################################
+    
+if __name__ == "__main__":
+    hi() 
