c79d49dfeb8a9c31d939bd69054fb7a5acba9b31
Jabba Laci <jabba.laci@gmail.com>
2011-09-19 01:08:47 -0400

    splinter

---
+++ demos/browser_automation/splinter_2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+"""
+splinter
+
+http://splinter.cobrateam.info
+"""
+
+from time import sleep
+
+#url = 'http://simile.mit.edu/crowbar/test.html'
+#url = 'http://dl.dropbox.com/u/144888/hello_js.html'
+url = 'http://www.ncbi.nlm.nih.gov/nuccore/CP002059.1'
+#url = 'http://translate.google.com/#en|fr|game'
+
+def main():
+    from splinter.browser import Browser
+
+    browser = Browser()
+    browser.visit(url)
+    
+    #browser.execute_script("var win = window.open(); win.document.write('<html><head><title>Generated HTML of  ' + location.href + '</title></head><pre>' + document.documentElement.innerHTML.replace(/&/g, '&amp;').replace(/</g, '&lt;') + '</pre></html>'); win.document.close(); void 0;")
+    
+    sleep(20)
+    
+    f = open("source.html", "w")
+    print >>f, browser.html
+    f.close()
+    
+    browser.quit()
+
+#############################################################################
+
+if __name__ == "__main__":
+    main()
