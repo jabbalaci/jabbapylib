cd6140ad51c9269cabd9f9292c8d2969eb56a31b
Jabba Laci <jabba.laci@gmail.com>
2012-01-17 20:55:32 +0100

    debug mode added

--- jabbapylib/audio/audio.py
+++ jabbapylib/audio/audio.py
@@ -9,9 +9,11 @@ Playing audio (and video too) files.
 import os
 import jabbapylib.config as cfg
 
-def play(audio_file, background=False):
+def play(audio_file, background=False, debug=False):
     """Play an audio file with mplayer."""
-    cmd = '{mplayer} {audio} 1>/dev/null 2>&1'.format(mplayer=cfg.MPLAYER, audio=audio_file)
+    cmd = '{mplayer} {audio}'.format(mplayer=cfg.MPLAYER, audio=audio_file)
+    if not debug:
+        cmd += ' 1>/dev/null 2>&1'
     if background:
         cmd += ' &'
     os.system(cmd)
