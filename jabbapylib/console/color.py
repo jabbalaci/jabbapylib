d3bffa405e8d4ad1e21f3123ec4008fad0e3a8d5
Jabba Laci <jabba.laci@gmail.com>
2012-02-24 12:24:31 +0100

    bold print added

--- jabbapylib/console/color.py
+++ jabbapylib/console/color.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
-# Recording soundcard output
-# https://ubuntuincident.wordpress.com/2011/03/26/recording-soundcard-output/
-# Laszlo Szathmary, 2011 (jabba.laci@gmail.com)
+"""
+Print colored text in the terminal.
+
+# from jabbapylib.console import color
+"""
 
 from jabbapylib.lib.termcolor import colored
 
@@ -10,9 +12,13 @@ def color(text, color, attrs=[]):
     """Produce a colored text for the terminal."""
     return colored(text, color, attrs=attrs)
 
+def bold(text, color='white'):
+    return colored(text, color, attrs=['bold'])
+
 ##############################################################################
 
 if __name__ == "__main__":
     print color('Everything is going perfectly fine.', 'green')
     print color('Reactor meltdown alert! Leave the building immediately!', 'red')
     print color('[ OK ]', 'cyan')
+    print bold("Bold white color", 'white')
