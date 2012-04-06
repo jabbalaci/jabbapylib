e3a7d9d19f3f256f25381cef73f835f007b665cf
Jabba Laci <jabba.laci@gmail.com>
2012-04-06 14:33:48 +0200

    vanity stat. added to upload script

--- 03_upload.sh
+++ 03_upload.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 python setup.py register sdist upload
+vanity jabbapylib
--- jabbapylib/config.py
+++ jabbapylib/config.py
@@ -7,8 +7,8 @@ Configuration part.
 """
 
 __author__ = "Laszlo Szathmary (jabba.laci@gmail.com)"
-__version__ = "0.1.9"
-__date__ = "20120405"
+__version__ = "0.2.0"
+__date__ = "20120406"
 __copyright__ = "Copyright (c) 2011-2012 Laszlo Szathmary"
 __license__ = "GPL"
 
