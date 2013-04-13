374ba75ac28b6624a2449d5f2c256de0d4b617de
Jabba Laci <jabba.laci@gmail.com>
2013-04-13 19:46:07 +0200

    docs updated

--- jabbapylib/clipboard/clipboard.py
+++ jabbapylib/clipboard/clipboard.py
@@ -4,7 +4,7 @@
 Copy text to clipboards (to both of them). 
 This solution here is specific to Linux.
 
-For a podium independent solution, you can check out
+For a platform independent solution, you can check out
 https://github.com/asweigart/mapitpy/blob/master/pyperclip.py 
 (I didn't try it).
 
