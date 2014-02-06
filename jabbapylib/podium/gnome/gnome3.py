9dbae254f82c81c05749aaf69c17db6cacbb6034
Jabba Laci <jabba.laci@gmail.com>
2014-02-06 17:56:51 +0100

    whitespaces

--- jabbapylib/podium/gnome/gnome3.py
+++ jabbapylib/podium/gnome/gnome3.py
@@ -13,19 +13,20 @@ from jabbapylib.process import process
 
 def set_wallpaper(img):
     """Set the given file as wallpaper."""
-    
+
     uri = 'file://' + img
     cmd = 'gsettings set org.gnome.desktop.background picture-uri {uri}'.format(uri=uri)
     call(shlex.split(cmd), stdout=PIPE)
-    
+
+
 def get_wallpaper():
     """Get the path of the file that is set as wallpaper."""
-    
+
     cmd = 'gsettings get org.gnome.desktop.background picture-uri'
     uri = process.get_simple_cmd_output(cmd)
     return uri.replace("'", "")
 
 #############################################################################
-    
+
 if __name__ == "__main__":
     print get_wallpaper()
